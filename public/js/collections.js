d82e2e04135d2760c671ee55d27e2ce28fb42991
Ismael <ismael.diaz2019@hotmail.com>
2019-08-08 11:03:05 -0500

    testing .card-add button

--- public/js/collections.js
+++ public/js/collections.js
@@ -335,6 +335,30 @@ $(document).ready(function () {
       //   console.log('done')
       // });
       console.log('.card-add is clicked - 20190808');
+      console.log(storage.getCollection());
+
+
+      // $.ajax({
+      //   url: '/rest/model/atg/commerce/order/purchase/CartModifierActor/addItemToBasket',
+      //   method: 'POST',
+      //   // dataType: 'json',
+      //   contentType: 'application/json',
+      //   data: JSON.stringify({
+      //     "formSubmissionData":[
+      //       {
+      //         "skuId": "16551583", // child
+      //         "quantity": 1,
+      //         "productId": "16551581", // parent
+      //         "hasVariations": true
+      //       }
+      //     ]
+      //   }),
+      //   success: function (data) {
+      //     console.log('success...');
+      //     console.log(data);
+      //   }
+      // });
+      
     });
 
     storage.on('collectionChange', function () {
